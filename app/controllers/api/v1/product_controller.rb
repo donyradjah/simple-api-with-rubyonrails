1faa33f78d311d40417f57891df9ef39ac1db21a
donyradjah <donyradjah@gmail.com>
2016-03-11 23:46:25 +0700

    Change Response

--- app/controllers/api/v1/product_controller.rb
+++ app/controllers/api/v1/product_controller.rb
@@ -2,19 +2,23 @@ class Api::V1::ProductController < ApplicationController
   def index
     @products = Product.all
     render json: @products
+    end
+  def show
+    @products = Product.find(params[:id])
+    render json: @products
   end
   def create
     @product = Product.new(product_name: params[:product_name], stock: params[:stock])
     if @product.save
-      render json: @product
+      render json: {status: 'successful'}
     else
       render json: @product.errors
     end
   end
   def update
-    @product = Product.where(id: params[:id]).first
-    if @product.update_attribute(product_name: params[:product_name], stock: params[:stock])
-      render json: @product
+    @product = Product.find(params[:id])
+    if @product.update(product_name: params[:product_name], stock: params[:stock])
+      render  json: {status: 'successful'}
     else
       render json: @product.errors
     end
