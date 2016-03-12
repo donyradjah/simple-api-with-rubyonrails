b93e5f47a4ffa095bbc8af4682deddd0092ea6b3
donyradjah <donyradjah@gmail.com>
2016-03-12 12:33:45 +0700

    Add Pagination

--- app/controllers/api/v1/product_controller.rb
+++ app/controllers/api/v1/product_controller.rb
@@ -1,34 +1,57 @@
 class Api::V1::ProductController < ApplicationController
   def index
-    @products = Product.all
-    render json: @products
+    if params[:page].nil?
+      @page = 1
+    else
+      @page = params[:page]
     end
+    @products = Product.all.order('created_at DESC').paginate(page: @page, per_page: 10)
+    if params[:page].to_i > 1
+      @from = (params[:page].to_i - 1) * 10 + 1
+    else
+      @from = 1
+    end
+    if params[:page].to_i == 1
+      @to = @products.length
+    else
+      if params[:page].to_i > 1
+        @to = @from + @products.length - 1
+      end
+    end
+
+    render json: {total: @products.length, per_page: 10, page: @page, from: @from, to: @to, data: @products}
+  end
+
   def show
     @products = Product.find(params[:id])
     render json: @products
   end
+
   def create
     @product = Product.new(product_name: params[:product_name], stock: params[:stock])
     if @product.save
-      render json: {status: 'successful'}
+      render json: {success: true, status: 'successful'}
     else
-      render json: @product.errors
+      render json: {success: false, error: @product.errors}
     end
   end
+
   def update
     @product = Product.find(params[:id])
     if @product.update(product_name: params[:product_name], stock: params[:stock])
-      render  json: {status: 'successful'}
+      render json: {success: true, status: 'successful'}
     else
-      render json: @product.errors
+      render json: {success: false, error: @product.errors}
     end
   end
+
   def destroy
     @product = Product.where(id: params[:id]).first
     if @product.destroy
-      render json: {status: 'successful'}
+      render json: {success: true, status: 'successful'}
     else
-      render json: @product.errors
+      render json: {success: false, error: @product.errors}
     end
   end
+
 end
